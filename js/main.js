b5254e645cec06d0df4ced1d3a9d6bafe7257b17
Jenna Murphy <jmurphy@mcad.edu>
2020-12-02 09:27:50 -0600

    css, js, html updates to get sticky nav

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-/* Set the width of the side navigation to 250px */
+window.addEventListener('DOMContentLoaded', () => {
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0) {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+      } else {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+      }
+    });
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll('section[id]').forEach((section) => {
+    observer.observe(section);
+  });
+
+});
+
+// document.getElementById("navbar").style.height=window.innerHeight;
+
 // function openNav() {
 // document.getElementById("mySidenav").style.width = "250px";
 // }
@@ -9,67 +30,67 @@
 // }
 
 // cache the navigation links
-var $navigationLinks = $('#mySidenav > a');
-// cache (in reversed order) the sections
-var $sections = $($(".section").get().reverse());
-
-// map each section id to their corresponding navigation link
-var sectionIdTonavigationLink = {};
-$sections.each(function() {
-    var id = $(this).attr('id');
-    sectionIdTonavigationLink[id] = $('#mySidenav > a[href=\\#' + id + ']');
-});
-
-// throttle function, enforces a minimum time interval
-function throttle(fn, interval) {
-    var lastCall, timeoutId;
-    return function () {
-        var now = new Date().getTime();
-        if (lastCall && now < (lastCall + interval) ) {
-            // if we are inside the interval we wait
-            clearTimeout(timeoutId);
-            timeoutId = setTimeout(function () {
-                lastCall = now;
-                fn.call();
-            }, interval - (now - lastCall) );
-        } else {
-            // otherwise, we directly call the function
-            lastCall = now;
-            fn.call();
-        }
-    };
-}
-
-function highlightNavigation() {
-    // get the current vertical position of the scroll bar
-    var scrollPosition = $(window).scrollTop();
-
-    // iterate the sections
-    $sections.each(function() {
-        var currentSection = $(this);
-        // get the position of the section
-        var sectionTop = currentSection.offset().top;
-
-        // if the user has scrolled over the top of the section
-        if (scrollPosition >= sectionTop) {
-            // get the section id
-            var id = currentSection.attr('id');
-            // get the corresponding navigation link
-            var $navigationLink = sectionIdTonavigationLink[id];
-            // if the link is not active
-            if (!$navigationLink.hasClass('active')) {
-                // remove .active class from all the links
-                $navigationLinks.removeClass('active');
-                // add .active class to the current link
-                $navigationLink.addClass('active');
-            }
-            // we have found our section, so we return false to exit the each loop
-            return false;
-        }
-    });
-}
-
-$(window).scroll( throttle(highlightNavigation,100) );
-
-// if you don't want to throttle the function use this instead:
-// $(window).scroll( highlightNavigation );
+// var $navigationLinks = $('#mySidenav > a');
+// // cache (in reversed order) the sections
+// var $sections = $($(".section").get().reverse());
+//
+// // map each section id to their corresponding navigation link
+// var sectionIdTonavigationLink = {};
+// $sections.each(function() {
+//     var id = $(this).attr('id');
+//     sectionIdTonavigationLink[id] = $('#mySidenav > a[href=\\#' + id + ']');
+// });
+//
+// // throttle function, enforces a minimum time interval
+// function throttle(fn, interval) {
+//     var lastCall, timeoutId;
+//     return function () {
+//         var now = new Date().getTime();
+//         if (lastCall && now < (lastCall + interval) ) {
+//             // if we are inside the interval we wait
+//             clearTimeout(timeoutId);
+//             timeoutId = setTimeout(function () {
+//                 lastCall = now;
+//                 fn.call();
+//             }, interval - (now - lastCall) );
+//         } else {
+//             // otherwise, we directly call the function
+//             lastCall = now;
+//             fn.call();
+//         }
+//     };
+// }
+//
+// function highlightNavigation() {
+//     // get the current vertical position of the scroll bar
+//     var scrollPosition = $(window).scrollTop();
+//
+//     // iterate the sections
+//     $sections.each(function() {
+//         var currentSection = $(this);
+//         // get the position of the section
+//         var sectionTop = currentSection.offset().top;
+//
+//         // if the user has scrolled over the top of the section
+//         if (scrollPosition >= sectionTop) {
+//             // get the section id
+//             var id = currentSection.attr('id');
+//             // get the corresponding navigation link
+//             var $navigationLink = sectionIdTonavigationLink[id];
+//             // if the link is not active
+//             if (!$navigationLink.hasClass('active')) {
+//                 // remove .active class from all the links
+//                 $navigationLinks.removeClass('active');
+//                 // add .active class to the current link
+//                 $navigationLink.addClass('active');
+//             }
+//             // we have found our section, so we return false to exit the each loop
+//             return false;
+//         }
+//     });
+// }
+//
+// $(window).scroll( throttle(highlightNavigation,100) );
+//
+// // if you don't want to throttle the function use this instead:
+// // $(window).scroll( highlightNavigation );
